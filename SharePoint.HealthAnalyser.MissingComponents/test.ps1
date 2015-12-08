0ef3b9cfb03d37b6e2d050608fdaf111a6c98ea6
NItish <csnitish@gmail.com>
2015-12-08 16:50:29 +0530

    Adding the source articles  that explaint he steps to fix health analyzer issues

--- SharePoint.HealthAnalyser.MissingComponents/test.ps1
+++ SharePoint.HealthAnalyser.MissingComponents/test.ps1
@@ -1,38 +1,12 @@
 <#
 	Test 
 #>
-function Check-CurrentPageForBadWebPart(){
-	[CmdletBinding()]
-	param(
-		
-		[Microsoft.SharePoint.SPFile] $File,
-		[Microsoft.SharePoint.SPWeb] $web				
-	)	
-	
-	#$wpManager = $file.GetLimitedWebPartManager([System.Web.UI.WebControls.WebParts.PersonalizationScope]::Shared)
-	$wpManager = $web.GetLimitedWebPartManager("http://redactie-slo-710.pggm-o.nl/Paginas/default.aspx",[System.Web.UI.WebControls.WebParts.PersonalizationScope]::Shared)
-	foreach($wp in $wpManager.WebParts){
-		write-host $file.Url
-		write-host $wp.ID
-		write-host $wp.DisplayTitle
-		write-host $wp.Title
-		write-host $wp.ChromeType
-		write-host $wp.HelpMode
-		write-host $wp.GetType().ToString() 
-		write-host $wp.WebBrowsableObject.ToString() 
-		write-host $wp.ZoneIndex
-		write-host $wp.Zone.ID
-		write-host $wp.Zone.Title
-	#Microsoft.SharePoint.WebPartPages.ErrorWebPart
-	}
-}
 
-$site = new-object Microsoft.SharePoint.SPSite("http://redactie-slo-710.pggm-o.nl/Paginas/default.aspx")
-$web = $site.OpenWeb()
-$file = $web.GetFile("http://redactie-slo-710.pggm-o.nl/Paginas/default.aspx")
-if($file){
-	Check-CurrentPageForBadWebPart -File $file -web $web
+function Get-ScriptLocation
+{
+	write-host $MyInvocation.PSCommandPath
+	write-host $MyInvocation.CommandOrigin
+	$scriptPath = split-path -parent $Script:MyInvocation.MyCommand.Path
+	write-host $scriptPath
 }
-else{
-	Write-Host "File could not be loaded"
-}
+Get-ScriptLocation
